9b773f3d6960e286d14a32d51426cd8b8b468b79
MaruLin <how7633818@gmail.com>
2023-08-15 11:19:35 +0800

    [function] MenuBuilder add two methods into menubuilder 1. constructMenu 2. buildmenu

--- MenuBuilder.cs
+++ MenuBuilder.cs
@@ -8,5 +8,45 @@ namespace Menu_Practice
 {
     internal class MenuBuilder
     {
+        private readonly Menu _menu;
+        public MenuBuilder() 
+        {
+            _menu = new Menu();
+        }
+
+        public void ConstructMenu()
+        {
+            MenuList menuList = new();
+            MenuOption menuOption = new("Start");
+            menuList.Push(menuOption);
+
+            MenuOption menuOption2 = new("Exit");
+            menuList.Push(menuOption2);
+
+            _menu.Push(menuList);
+
+            MenuList menuList2 = new();
+            MenuOption menuOption1 = new("Lord");
+            menuList2.Push(menuOption1);
+
+            MenuOption menuOption3 = new("Deceiver");
+            menuList2.Push(menuOption3);
+            menuList2.AddParent(menuList);
+
+            _menu.Push(menuList2);
+        }
+
+        public Menu BuildMenu()
+        {
+            try
+            {
+                _menu.Initialize();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return _menu;
+        }
     }
 }
