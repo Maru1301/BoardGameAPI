293f417bc091f83401324a74453c4b23ecf6c66e
MaruLin <how7633818@gmail.com>
2023-08-15 11:39:45 +0800

    [Add] MenuManipulator field 1. _menu 2. _chooser

---
+++ MenuManipulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_Practice
+{
+    internal class MenuManipulator
+    {
+        private Menu _menu;
+        private int _chooser;
+
+        public MenuManipulator(Menu menu)
+        {
+            this._menu = menu;
+            _chooser = 0;
+        }
+
+        
+    }
+}
