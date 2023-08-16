c63b40d5e9c875e7eb3bd9a5952632c9123b816e
MaruLin <how7633818@gmail.com>
2023-08-16 11:02:57 +0800

    [function] MenuManipulator add methods 1. ActivateMenu 2. ReadUserInput 3. Show

--- MenuManipulator.cs
+++ MenuManipulator.cs
@@ -17,6 +17,66 @@ namespace Menu_Practice
             _chooser = 0;
         }
 
-        
+        public void ActivateMenu()
+        {
+            bool NeedtoChangeView = true;
+            while (true)
+            {
+                if (NeedtoChangeView)
+                {
+                    Show();
+                }
+                NeedtoChangeView = ReadUserInput();
+            }
+        }
+
+        private bool ReadUserInput()
+        {
+            bool changed = false;
+            ConsoleKey key = Console.ReadKey().Key;
+            if(key != ConsoleKey.Escape)
+            {
+                if(key == ConsoleKey.Enter)
+                {
+                    changed = true;
+                }
+                else if(key == ConsoleKey.UpArrow)
+                {
+                    if(_chooser > 0)
+                    {
+                        _chooser--;
+                        changed = true;
+                    }
+                }
+                else if(key == ConsoleKey.DownArrow)
+                {
+                    if(_chooser < _menu.GetCurrentList().Options.Count-1)
+                    {
+                        _chooser++;
+                        changed = true;
+                    }
+                }
+            }
+
+            return changed;
+        }
+
+        private void Show()
+        {
+            Console.Clear();
+
+            var list = _menu.GetCurrentList();
+            for(int i = 0; i < list.Options.Count; i++)
+            {
+                if(_chooser == i)
+                {
+                    Console.WriteLine($"=>  {list.Options[i].OptionName}");
+                }
+                else
+                {
+                    Console.WriteLine($"    {list.Options[i].OptionName}");
+                }
+            }
+        }
     }
 }
