958ed71688506e3f9919edfdcbc8d7fe3b5fb9b3
MaruLin <how7633818@gmail.com>
2023-08-18 15:38:05 +0800

    [add] CharacterInfoMenu Character

---
+++ CharacterInfoMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_Practice
+{
+    internal class CharacterInfoMenu : MenuList
+    {
+        private readonly Character _character = new();
+
+        public Character Character { get => _character; }
+
+        public CharacterInfoMenu(Character character)
+        {
+            _character = character;
+        }
+    }
+}
