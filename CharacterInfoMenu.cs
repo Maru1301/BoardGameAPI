bb42432cf10de7013bed8dec23b32b3c0dfc8aed
MaruLin <how7633818@gmail.com>
2023-08-20 20:11:52 +0800

    [add] method in CharacterInfoMenu 1. ShowInfo

--- CharacterInfoMenu.cs
+++ CharacterInfoMenu.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Menu_Practice.Characters;
 
 namespace Menu_Practice
 {
@@ -15,6 +18,13 @@ namespace Menu_Practice
         public CharacterInfoMenu(Character character)
         {
             _character = character;
+        }
+
+        public override void ShowInfo()
+        {
+            string info = string.Empty;
+            info += $"{_character.Name}\r\n使用規則: [{_character.Rule}]\r\n手牌: (皇冠 X {_character.Cards[0]}, 盾牌 X {_character.Cards[1]}, 匕首 X {_character.Cards[2]})\r\n失格條件: {_character.DisqualificationCondition}\r\n晉升條件: {_character.EvolutionCondition}\r\n額外分數: {_character.AdditionalPointCondition}";
+            Console.WriteLine(info);
         }
     }
 }
