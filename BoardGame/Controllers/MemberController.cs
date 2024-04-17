395270ee1ca0da5fecb8647d7db782e2934506b4
MaruLin <how7633818@gmail.com>
2024-04-17 10:58:37 +0800

    Create BoardGame branch

---
+++ BoardGame/Controllers/MemberController.cs
@@ -0,0 +1,30 @@
+﻿using BoardGame.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace BoardGame.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MemberController : ControllerBase
+    {
+        private readonly MemberService _memberService;
+
+        public MemberController(MemberService memberService)
+        {
+            _memberService = memberService;
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Register()
+        {
+            var result = _memberService.Register(member.ToMemberDTO(), urlTemplate);
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok(result.Message);
+        }
+    }
+}
---
+++ BoardGame/Services/MemberService.cs
@@ -0,0 +1,38 @@
+﻿namespace BoardGame.Services
+{
+    public class MemberService
+    {
+        public (bool Success, string Message) Register(MemberRegisterDTO dto, string urlTemplate)
+        {
+            if (_memberRepository.IsExist(dto.MemberAccount!))
+            {
+                return (false, "帳號已存在");
+            }
+            // 驗證暱稱是否重複
+            if (_memberRepository.NickNameExist(dto.MemberNickName!))
+            {
+                return (false, "暱稱已存在");
+            }
+
+            if (_memberRepository.EmailExist(dto.MemberEmail!))
+            {
+                return (false, "信箱已存在");
+            }
+
+            dto.ConfirmCode = Guid.NewGuid().ToString("N");
+
+            _memberRepository.MemberRegister(dto);
+
+            MemberDTO entity = _memberRepository.GetByAccount(dto.MemberAccount);
+            // 發email
+            string url = string.Format(urlTemplate, entity.Id, dto.ConfirmCode);
+
+            new EmailHelper().SendConfirmRegisterEmail(url, dto.MemberNickName!, dto.MemberEmail!);
+
+            // 創建播放佇列
+            _queueRepository.CreateQueue(entity.Id);
+
+            return (true, "註冊成功，已發送驗證信");
+        }
+    }
+}
