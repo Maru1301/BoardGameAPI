e4906bf80c4ae98989ee142550c9997b875c693b
MaruLin <how7633818@gmail.com>
2024-04-26 15:23:41 +0800

    update JWTHelper parameter naming

--- BoardGame/Infrastractures/JWTHelper.cs
+++ BoardGame/Infrastractures/JWTHelper.cs
@@ -14,17 +14,17 @@ namespace BoardGame.Infrastractures
             _settings = settings.CurrentValue;
         }
 
-        public string GenerateToken(string userName, string role, int expireMinutes = 120)
+        public string GenerateToken(string account, string role, int expireMinutes = 120)
         {
             var issuer = _settings.ValidIssuer;
             var signKey = _settings.Secret;
 
-            var token = BuildToken(userName, role, expireMinutes, issuer, signKey);
+            var token = BuildToken(account, role, expireMinutes, issuer, signKey);
 
             return token;
         }
 
-        private static string BuildToken(string userName,string role, int expireMinutes, string issuer, string signKey)
+        private static string BuildToken(string account, string role, int expireMinutes, string issuer, string signKey)
         {
             var jwtBuilder = JwtBuilder.Create()
                 .WithAlgorithm(new HMACSHA256Algorithm())
@@ -32,11 +32,11 @@ namespace BoardGame.Infrastractures
                 .AddClaim("Roles", role)
                 .AddClaim("jti", Guid.NewGuid().ToString())
                 .AddClaim("iss", issuer)
-                .AddClaim("sub", userName)
+                .AddClaim("sub", account)
                 .AddClaim("exp", GetExpirationTime(expireMinutes))
                 .AddClaim("nbf", GetCurrentTime())
                 .AddClaim("iat", GetCurrentTime())
-                .AddClaim(ClaimTypes.Name, userName);
+                .AddClaim(ClaimTypes.Name, account);
 
             return jwtBuilder.Encode();
         }
