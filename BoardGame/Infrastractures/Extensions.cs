429e9c9bd48a4e5091d9ce576998c4a6c0d1e20c
MaruLin <how7633818@gmail.com>
2024-05-30 13:26:32 +0800

    Update: Add summary to Extensions.To<TResuslt>()

--- BoardGame/Infrastractures/Extensions.cs
+++ BoardGame/Infrastractures/Extensions.cs
@@ -5,14 +5,21 @@ namespace BoardGame.Infrastractures
 {
     public static class Extensions
     {
-        public static T To<T>(this object source) where T : new()
+        /// <summary>
+        /// Attempts to convert an object of any type to a specified target type <typeparamref name="TResult"/> using AutoMapper.
+        /// </summary>
+        /// <typeparam name="TResult">The target type to which the object should be converted.</typeparam>
+        /// <param name="source">The object to be converted.</param>
+        /// <returns>An instance of the target type <typeparamref name="TResult"/> populated with the mapped values from the source object, 
+        /// or null if the conversion fails.</returns>
+        public static TResult To<TResult>(this object source) where TResult : new()
         {
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap(source.GetType(), typeof(T));
+                cfg.CreateMap(source.GetType(), typeof(TResult));
             });
             IMapper mapper = config.CreateMapper();
-            var target = mapper.Map<T>(source);
+            var target = mapper.Map<TResult>(source);
             return target;
         }
 
