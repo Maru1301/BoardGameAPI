3ee431c19303b850275d3cdc7eb3323c92727023
MaruLin <how7633818@gmail.com>
2024-05-31 17:50:48 +0800

    Add: Integration tests
    Add ApiTests for Member/Register and Member/Delete
    Add FakeStartup for ApiTests

---
+++ BoardGame/FakeStartup.cs
@@ -0,0 +1,127 @@
+﻿using BoardGame.Filters;
+using BoardGame.Infrastractures;
+using BoardGame.Models.EFModels;
+using BoardGame.Services.Interfaces;
+using BoardGame.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+
+namespace BoardGame
+{
+    public class FakeStartup(IConfiguration configuration)
+    {
+        public IConfiguration Configuration { get; } = configuration;
+
+        private static void ConfigureServices(IServiceCollection services, Microsoft.Extensions.Configuration.ConfigurationManager configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowEveryone", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+
+            // Clear the default mapping
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            // Register JwtHelper
+            services.AddScoped<JWTHelper>();
+            // Register using option mode
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            services.Configure<JWTSettingsOptions>(jwtSettings);
+            // Set authentication method
+            services
+              // Authenticate using bearer token and jwt format for token
+              .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options => {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      // Allow [Authorize] to determine roles
+                      RoleClaimType = ClaimTypes.Role,
+                      // Validate the issuer
+                      ValidateIssuer = true,
+                      ValidIssuer = jwtSettings.GetValue<string>("ValidIssuer"),
+                      // Validate the audience
+                      ValidateAudience = true,
+                      ValidAudience = jwtSettings.GetValue<string>("ValidAudience"),
+                      //Validate only if the token contains a key, usually only the signature
+                      ValidateIssuerSigningKey = true,
+                      // Key used for the signature
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.GetValue<string>("IssuerSigningKey") ?? string.Empty))
+                  };
+              });
+
+            services.AddControllers();
+            RegisterScopedServices(services);
+
+            var mongoDBSettings = configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseMongoDB(mongoDBSettings?.AtlasURI ?? "", mongoDBSettings?.DatabaseName ?? ""), ServiceLifetime.Scoped);
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                // Describe how the api is protected
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    // When type is chosen as http, the Bearer prefix can be omitted when making authentication through the swagger interface (as shown in the figure below)
+                    Type = SecuritySchemeType.Http,
+                    // Use Bearer token
+                    Scheme = "Bearer",
+                    // Bearer format uses jwt
+                    BearerFormat = "JWT",
+                    // Authentication is placed in the header of the http request
+                    In = ParameterLocation.Header,
+                    // Description
+                    Description = "JWT authentication description"
+                });
+                // Create additional filters to filter Authorize, AllowAnonymous, or even those without attributes
+                options.OperationFilter<AuthorizeCheckOperationFilter>();
+            });
+        }
+
+        private static void RegisterScopedServices(IServiceCollection services)
+        {
+            //register UoW
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //register redis service
+            services.AddScoped<ICacheService, CacheService>();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var types = assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Any(i => i.Name == "IService" || i.Name == "IRepository"));
+
+            foreach (var type in types)
+            {
+                var interfaces = type.GetInterfaces().Where(i => i.Name != "IService" || i.Name != "IRepository");
+
+                foreach (var interfaceType in interfaces)
+                {
+                    services.AddScoped(interfaceType, type);
+                }
+            }
+        }
+
+        private static void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("AllowEveryone");
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
---
+++ BoardGameTest/ApiTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using BoardGame;
+using RestSharp;
+using Shouldly;
+using BoardGame.Models.DTOs;
+using Microsoft.Extensions.DependencyInjection;
+using BoardGame.Infrastractures;
+
+namespace BoardGameTest
+{
+    public class ApiTests(WebApplicationFactory<FakeStartup> factory) : IClassFixture<WebApplicationFactory<FakeStartup>>
+    {
+        [Theory]
+        [InlineData("/api/Member/Register")]
+        public async Task RegisterTest(string url)
+        {
+            using var client = new RestClient(factory.CreateClient());
+            var request = new RestRequest(url);
+            request.AddBody(new RegisterRequestDTO
+            {
+                Name = "unit test",
+                Account = "test",
+                Password = "test",
+                ConfirmPassword = "test",
+                Email = "test@test.com",
+            });
+
+            var response = await client.PostAsync(request);
+
+            response.ShouldNotBeNull();
+        }
+
+        [Theory]
+        [InlineData("/api/Member/Delete")]
+        public async Task DeleteMemberTest(string url)
+        {
+            using var client = new RestClient(factory.CreateClient());
+            var request = new RestRequest(url);
+            var objectId = "66598af45c522ca57654047e";
+            request.AddQueryParameter("objectId", objectId);
+
+            using var scope = factory.Services.CreateScope();
+            var jwtHelper = scope.ServiceProvider.GetRequiredService<JWTHelper>();
+            var jwt = jwtHelper.GenerateToken(new MongoDB.Bson.ObjectId(objectId), "", Role.Admin);
+            request.AddHeader("Authorization", $"Bearer {jwt}");
+
+            var response = await client.DeleteAsync(request);
+
+            response.ShouldNotBeNull();
+        }
+    }
+}
