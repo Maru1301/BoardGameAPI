73d80bce21e52c91e4cbdf4810a566f1d314c161
MaruLin <how7633818@gmail.com>
2024-06-14 13:33:20 +0800

    Update: Remove redundant class and make hardcoded string inlinedata

--- BoardGameTest/ApiTests.cs
+++ BoardGameTest/ApiTests.cs
@@ -13,12 +13,11 @@ namespace BoardGameTest
     public class ApiTests(WebApplicationFactory<FakeStartup> factory) : IClassFixture<WebApplicationFactory<FakeStartup>>
     {
         [Theory, TestPriority(100)]
-        [InlineData("/api/Member/GetMemberInfo")]
-        public async Task Test01_GetMemberInfoTest(string url)
+        [InlineData("/api/Member/GetMemberInfo", "66262710924804b2f0c40e2a")]
+        public async Task Test01_GetMemberInfoTest(string url, string id)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
-            var id = "66262710924804b2f0c40e2a";
 
             using var scope = factory.Services.CreateScope();
             var jwtHelper = scope.ServiceProvider.GetRequiredService<JWTHelper>();
@@ -31,13 +30,12 @@ namespace BoardGameTest
         }
 
         [Theory, TestPriority(90)]
-        [InlineData("/api/Member/ListMembers")]
-        public async Task Test02_ListMembersTest(string url)
+        [InlineData("/api/Member/ListMembers", "66262710924804b2f0c40e2a")]
+        public async Task Test02_ListMembersTest(string url, string id)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
-            var id = "66262710924804b2f0c40e2a";
-
+            
             using var scope = factory.Services.CreateScope();
             var jwtHelper = scope.ServiceProvider.GetRequiredService<JWTHelper>();
             var jwt = jwtHelper.GenerateToken(new MongoDB.Bson.ObjectId(id), "", Role.Admin);
@@ -49,18 +47,18 @@ namespace BoardGameTest
         }
 
         [Theory, TestPriority(80)]
-        [InlineData("/api/Member/Register")]
-        public async Task Test03_RegisterTest(string url)
+        [InlineData("/api/Member/Register", "unit test", "test", "test", "test", "93220allen@gmail.com")]
+        public async Task Test03_RegisterTest(string url, string name, string account, string password, string confirmPassword, string email)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
             request.AddBody(new RegisterRequestDTO
             {
-                Name = "unit test",
-                Account = "test",
-                Password = "test",
-                ConfirmPassword = "test",
-                Email = "93220allen@gmail.com",
+                Name = name,
+                Account = account,
+                Password = password,
+                ConfirmPassword = confirmPassword,
+                Email = email,
             });
 
             var response = await client.PostAsync(request);
@@ -69,15 +67,15 @@ namespace BoardGameTest
         }
 
         [Theory, TestPriority(70)]
-        [InlineData("/api/Member/Login")]
-        public async Task Test04_LoginTest(string url)
+        [InlineData("/api/Member/Login", "93220allen", "allen93220")]
+        public async Task Test04_LoginTest(string url, string account, string password)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
             request.AddBody(new MemberLoginRequestDTO
             {
-                Account = "93220allen",
-                Password = "allen93220",
+                Account = account,
+                Password = password
             });
 
             var response = await client.PostAsync(request);
@@ -87,12 +85,11 @@ namespace BoardGameTest
 
         
         [Theory, TestPriority(60)]
-        [InlineData("/api/Member/ResendConfirmationCode")]
-        public async Task Test05_ResendConfirmationCodeTest(string url)
+        [InlineData("/api/Member/ResendConfirmationCode", "663893baa37c3d3fab56755f")]
+        public async Task Test05_ResendConfirmationCodeTest(string url, string id)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
-            var id = "663893baa37c3d3fab56755f";
 
             using var scope = factory.Services.CreateScope();
             var jwtHelper = scope.ServiceProvider.GetRequiredService<JWTHelper>();
@@ -106,17 +103,16 @@ namespace BoardGameTest
 
         
         [Theory, TestPriority(50)]
-        [InlineData("/api/Member/EditMemberInfo")]
-        public async Task Test06_EditMemberInfoTest(string url)
+        [InlineData("/api/Member/EditMemberInfo", "test01", "test@example.com", "663893baa37c3d3fab56755f")]
+        public async Task Test06_EditMemberInfoTest(string url, string name, string email, string id)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
             request.AddBody(new EditRequestDTO
             {
-                Name = "test01",
-                Email = "test@example.com"
+                Name = name,
+                Email = email
             });
-            var id = "663893baa37c3d3fab56755f";
 
             using var scope = factory.Services.CreateScope();
             var jwtHelper = scope.ServiceProvider.GetRequiredService<JWTHelper>();
@@ -130,17 +126,16 @@ namespace BoardGameTest
 
         
         [Theory, TestPriority(40)]
-        [InlineData("/api/Member/ResetPassword")]
-        public async Task Test07_ResetPasswordTest(string url)
+        [InlineData("/api/Member/ResetPassword", "123456", "123456", "663893baa37c3d3fab56755f")]
+        public async Task Test07_ResetPasswordTest(string url, string oldPassword, string newPassword, string id)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
             request.AddBody(new ResetPasswordRequestDTO
             {
-                OldPassword = "123456",
-                NewPassword = "123456"
+                OldPassword = oldPassword,
+                NewPassword = newPassword
             });
-            var id = "663893baa37c3d3fab56755f";
 
             using var scope = factory.Services.CreateScope();
             var jwtHelper = scope.ServiceProvider.GetRequiredService<JWTHelper>();
@@ -154,13 +149,13 @@ namespace BoardGameTest
 
         
         [Theory, TestPriority(30)]
-        [InlineData("/api/Member/ValidateEmail")]
-        public async Task Test08_ValidateEmailTest(string url)
+        [InlineData("/api/Member/ValidateEmail", "66388e053d17012a98f9222e", "906e72acaf18450ea0b7249494b7c114")]
+        public async Task Test08_ValidateEmailTest(string url, string memberId, string confirmationCode)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
-            request.AddParameter("memberId", "66388e053d17012a98f9222e");
-            request.AddParameter("confirmationCode", "906e72acaf18450ea0b7249494b7c114");
+            request.AddParameter("memberId", memberId);
+            request.AddParameter("confirmationCode", confirmationCode);
 
             var response = await client.GetAsync(request);
 
@@ -168,12 +163,11 @@ namespace BoardGameTest
         }
 
         [Theory, TestPriority(20)]
-        [InlineData("/api/Member/Delete")]
-        public async Task Test09_DeleteMemberTest(string url)
+        [InlineData("/api/Member/Delete", "test")]
+        public async Task Test09_DeleteMemberTest(string url, string account)
         {
             using var client = new RestClient(factory.CreateClient());
             var request = new RestRequest(url);
-            var account = "test";
             request.AddQueryParameter("account", account);
 
             using var scope = factory.Services.CreateScope();
